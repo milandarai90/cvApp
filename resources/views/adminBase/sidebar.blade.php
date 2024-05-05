2c74b5b3f455a3befdaeedca8070667749c60d1e
milandarai90 <milandarai90@gmail.com>
2024-05-05 13:50:15 +0545

    first commit

---
+++ resources/views/adminBase/sidebar.blade.php
@@ -0,0 +1,100 @@
+
+          
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark" style="position: sticky"> <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="/" class="brand-link "> <!--begin::Brand Image--> <img src="" alt="" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-bold">cvApp</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+    
+    <div class="sidebar-wrapper">
+        <nav class="mt-2"> <!--begin::Sidebar Menu-->
+       
+            <ul class="nav sidebar-menu flex-column " data-lte-toggle="treeview" data-accordion="false">
+                <li class="nav-item">       
+                        <a href="" class="nav-link" id="dashboard">
+                            <i class="nav-icon bi bi-speedometer"></i>
+                            <p class="fw-bold">Dashboard</p>
+                        </a>
+                    </a>
+                </li>
+              
+                <li class="nav-item" id="country"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-globe2"></i>
+                    <p class="fw-bold">
+                  ...
+                        <i class="nav-arrow bi bi-chevron-right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" id="addCountry">
+                    <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-plus-square"></i>
+                            <p>...</p>
+                        </a> </li>
+                </ul>
+                <ul class="nav nav-treeview" id="viewCountry">
+                    <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-view-list"></i>
+                            <p>...</p>
+                        </a> </li>
+                </ul>
+            </li>
+         
+                <li class="nav-item " id="videoLink"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-camera-reels"></i>
+                        <p class="fw-bold">
+                           ...
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" id="addVideoLink">
+                        <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-plus-square"></i>
+                                <p>...</p>
+                            </a> </li>
+                    </ul>
+                    <ul class="nav nav-treeview" id="viewVideoLink">
+                        <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-view-list"></i>
+                                <p>...</p>
+                            </a> </li>
+                    </ul>
+                </li>
+                <li class="nav-item " id="description"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-journal-text"></i>
+                    <p class="fw-bold">
+                        ...
+                        <i class="nav-arrow bi bi-chevron-right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" id="addDescription">
+                    <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-plus-square"></i>
+                            <p>...</p>
+                        </a> </li>
+                </ul>
+                <ul class="nav nav-treeview" id="viewDescription">
+                    <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-view-list"></i>
+                            <p>...</p>
+                        </a> </li>
+                </ul>
+            </li>
+            <li class="nav-item " id="page"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-incognito"></i>
+                <p class="fw-bold">
+                    ...
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview" id="addpage">
+                <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-plus-square"></i>
+                        <p>...</p>
+                    </a> </li>
+            </ul>
+            <ul class="nav nav-treeview" id="viewpage">
+                <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-view-list"></i>
+                        <p>...</p>
+                    </a> </li>
+            </ul>
+            <ul class="nav nav-treeview" id="addindex">
+                <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-plus-square"></i>
+                        <p>... </p>
+                    </a> </li>
+            </ul><ul class="nav nav-treeview" id="viewIndex">
+                <li class="nav-item"> <a href="" class="nav-link"> <i class="nav-icon bi bi-view-list"></i>
+                        <p>...</p>
+                    </a> </li>
+            </ul>
+        </li>
+                </ul>
+        </nav>
+    </div>
+    </aside> 
+
